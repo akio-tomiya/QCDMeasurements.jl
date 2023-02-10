fd1f4570ea0b3d0350d511913e4656d3802b65d2
Yuki Nagai <cometscome@gmail.com>
2023-02-10 12:23:06 +0900

    pion

--- test/gauge.jl
+++ test/gauge.jl
@@ -16,5 +16,26 @@ function SU3test()
     i = 1
     L = [NX,NY,NZ,NT]
     load_gaugefield!(U,i,ildg,L,NC)
+
+    m_plaq = Plaquette_measurement(U)
+    m_poly = Polyakov_measurement(U)
+    m_pion = Pion_correlator_measurement(U)
+    m_pion_Staggered = Pion_correlator_measurement(U,fermiontype = "Staggered")
+    m_pion_Wilson = Pion_correlator_measurement(U,fermiontype = "Wilson")
+
+    plaq = get_value(measure(m_plaq,U))
+    poly = get_value(measure(m_poly,U))
+    println("plaq: $plaq")
+    println("poly: $poly")
+
+
+    pion = get_value(measure(m_pion,U))
+    pion_s = get_value(measure(m_pion_Staggered,U))
+    pion_w = get_value(measure(m_pion_Wilson,U))
+
+    println("pion: $pion")
+    println("pion correlator with Staggered fermion: $pion_s")
+    println("pion correlator with  Wilson fermion: $pion_w")
+
 end
 SU3test()
